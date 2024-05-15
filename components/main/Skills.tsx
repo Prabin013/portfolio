b71d227d1366fade55a2c3f5694939bdace0cad5
padhikari2 <adhikariprabin001@gmail.com>
2024-05-15 09:50:36 -0600

    finished the general need to modify and make it my own

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -5,7 +5,7 @@ import SkillText from '../sub/SkillText'
 
 const Skills = () => {
   return (
-    <section className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
+    <section className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20'
       style={{transform: "scale(0.9"}}
       >
         <SkillText />
